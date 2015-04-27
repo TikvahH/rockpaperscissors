8f00c0db3807c5ea086c99515bc0da81149bb340
Tikvah <T.harr@live.com>
2015-04-27 11:31:08 -0400

    Completed Rock Paper Scissors exercise.

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -1,55 +1,134 @@
-////////////////////////////////////////////////
-/*   Provided Code - Please Don't Edit   */
-////////////////////////////////////////////////
-'use strict';
+$(document).ready(function(){
 
-function getInput() {
-    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
-    return prompt();
-}
-function randomPlay() {
-    var randomNumber = Math.random();
-    if (randomNumber < 0.33) {
-        return "rock";
-    } else if (randomNumber < 0.66) {
-        return "paper";
-    } else {
-        return "scissors";
+$('#rock').on('click', function(){
+   console.log('rock');
+   playedRock();
+
+});
+
+function playedRock(){
+//get the computers play	
+var computerPlay = getComputerPlay();
+
+//compare my play with the computers
+if(computerPlay == 'paper'){
+    //computer won
+    $('#status').html('The computer won. You played rock. The computer played paper.');
+
+    //add to the computers score
+    var score = $('#computerScore').html();
+    score = parseFloat(score);
+    score +=1;
+    $('#computerScore').html(score);
+  }else if (computerPlay == 'scissors'){
+
+  	//you won
+  	$('#status').html('You won. You played rock. The computer played scissors.');
+  	//add the human score
+  	var score = $('#humanScore').html();
+  	score = parseFloat(score);
+  	score += 1;
+  	$('#humanScore').html(score).html;
+
+  }else{
+  	//you tied
+  	$('#status').html('You tied. You both played rock.');
+  		
+
     }
-}
-////////////////////////////////////////////////
-/*           Write Your Code Below            */
-////////////////////////////////////////////////
 
-function getPlayerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
-}
+}// end function played Rock
 
-function getComputerMove(move) {
-    // Write an expression that operates on a variable called `move`
-    // If a `move` has a value, your expression should evaluate to that value.
-    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-}
+$('#paper').on('click', function(){
+   console.log('paper');
+   playedPaper();
 
-function getWinner(playerMove,computerMove) {
-    var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
-    return winner;
-}
+});
 
-function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
-}
+function playedPaper(){
+//get the computers play  
+var computerPlay = getComputerPlay();
 
+//compare my play with the computers
+if(computerPlay == 'paper'){
+    //you tied
+    $('#status').html('You tied. You played paper. And the computer played paper.');
+
+    //add to the computers score
+  
+  }else if (computerPlay == 'scissors'){
+
+    //computer wins
+    $('#status').html('You played paper. The computer played scissors. The computer wins.');
+   
+    //add the human score
+    var score = $('#computerScore').html();
+    score = parseFloat(score);
+    score += 1;
+    $('#computerScore').html(score).html;
+
+  }else{
+    //you win
+    $('#status').html('You win. The computer played rock. You played paper.');
+
+    var score = $('#humanScore').html();
+    score = parseFloat(score);
+    score += 1;
+    $('#humanScore').html(score).html;
+
+  }
+}// end played paper
+
+$('#scissors').on('click', function(){
+   console.log('scissors');
+   playedScissors();
+
+});
+
+function playedScissors(){
+//get the computers play  
+var computerPlay = getComputerPlay();
+
+//compare my play with the computers
+if(computerPlay == 'playedScissors'){
+    //You tied
+    $('#status').html(' You tied. You played scissors. And the computer played scissors.');
+
+    //add to the computers score
+  
+  }else if (computerPlay == 'scissors'){
+
+    //computer wins
+    $('#status').html('Computer wins. You played scissors. The computer played rock.');
+   
+    //add the human score
+    var score = $('#computerScore').html();
+    score = parseFloat(score);
+    score += 1;
+    $('#computerScore').html(score).html;
+
+  }else{
+    //you win
+    $('#status').html('You win. The computer played paper. You played scissors.');
+
+    var score = $('#humanScore').html();
+    score = parseFloat(score);
+    score += 1;
+    $('#humanScore').html(score).html;
+
+  }
+}//end played scissors
+
+
+function getComputerPlay() {
+   var plays = ['rock', 'paper', 'scissors'];
+   var play = plays[Math.floor(Math.random()* plays.length)];
+   return play;
+}
+    
+
+
+
+
+
+})
